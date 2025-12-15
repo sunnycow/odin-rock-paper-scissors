d1164fa944f84a2e55860a025dd335779ec2f880
sunnycow <sannikov_yao@farpost.com>
2025-12-15 18:26:52 +1000

    Add getHumanChoice function

--- game.js
+++ game.js
@@ -2,3 +2,13 @@ function getComputerChoice() {
   const variants = ["rock", "paper", "scissors"];
   return variants[Math.floor(Math.random() * 3)];
 }
+
+function getHumanChoice() {
+  const userChoice = prompt("enter your choice (rock, paper or scissors)").toLowerCase();
+
+  if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+    return userChoice;
+  } else {
+    throw new Error("Invalid value");
+  }
+}
