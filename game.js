2245d2a2a4f53f27af5df0b9c24636a7bdccdf07
sunnycow <sannikov_yao@farpost.com>
2025-12-15 19:15:57 +1000

    Add playRound fuction

--- game.js
+++ game.js
@@ -12,3 +12,50 @@ function getHumanChoice() {
     throw new Error("Invalid value");
   }
 }
+
+let userScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+    return;
+  }
+
+  if (humanChoice === "rock") {
+    if (computerChoice === "scissors") {
+      console.log("You win! Rock beats Scissors");
+      userScore++;
+    } else {
+      console.log("You lose! Paper beats Rock");
+      computerScore++;
+    }
+  }
+
+  if (humanChoice === "paper") {
+    if (computerChoice === "rock") {
+      console.log("You win! Paper beats Rock");
+      userScore++;
+    } else {
+      console.log("You lose! Scissors beat Paper");
+      computerScore++;
+    }
+  }
+
+  if (humanChoice === "scissors") {
+    if (computerChoice === "paper") {
+      console.log("You win! Scissors beat Paper");
+      userScore++;
+    } else {
+      console.log("You lose! Rock beats Scissors");
+      computerScore++;
+    }
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
